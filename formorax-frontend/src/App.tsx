b0201c0a32527b140c4651dcbe77c3e23748e7ea
Kaviya-1508 <kaviya.kavi15082006@gmail.com>
2026-04-23 09:37:23 +0530

    Fix: Make form view a public route

--- formorax-frontend/src/App.tsx
+++ formorax-frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
 import FormBuilder from './pages/FormBuilder';
-import FormView from './pages/FormView';
+import FormView from './pages/FormView'; // Make sure this is imported
 import Responses from './pages/Responses';
 import ProtectedRoute from './components/ProtectedRoute';
 
@@ -11,10 +11,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* 1. Public Routes (No login required) */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/form/:slug" element={<FormView />} />
+        <Route path="/form/:slug" element={<FormView />} />  {/* ✅ This MUST be outside ProtectedRoute */}
 
+        {/* 2. Protected Routes (Login required) */}
         <Route element={<ProtectedRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/forms/new" element={<FormBuilder />} />
@@ -22,6 +24,7 @@ function App() {
           <Route path="/forms/:id/responses" element={<Responses />} />
         </Route>
 
+        {/* 3. Default Redirect */}
         <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="*" element={<Navigate to="/dashboard" />} />
       </Routes>
